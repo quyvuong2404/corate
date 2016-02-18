787378170da7cda59e78a3b120cc92426a1e24b6
vuong <valvuong2201@gmail.com>
2016-02-18 10:20:21 +0700

    commit 18/2

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,7 @@ module.exports = {
 	'googleAuth': {
 		'clientID': '616051613051-ihk8f8h4go370t5542nu45aa3cggu5sp.apps.googleusercontent.com',
 		'clientSecret': '_oz-7eB3zVpApOwXzun7xWDE',
-		'callbackURL': 'http://localhost:3000/auth/google/callback'
+		// 'callbackURL': 'http://localhost:3000/auth/google/callback'
+		'callbackURL': 'http://161.202.33.157:3000/auth/google/callback'
 	}
 }
