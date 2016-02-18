59fdd5930933d57587faab01f2856bf70e5222b0
vuong <valvuong2201@gmail.com>
2016-02-18 12:16:38 +0700

    commit 18/2

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
 		'clientID': '616051613051-ihk8f8h4go370t5542nu45aa3cggu5sp.apps.googleusercontent.com',
 		'clientSecret': '_oz-7eB3zVpApOwXzun7xWDE',
 		// 'callbackURL': 'http://localhost:3000/auth/google/callback'
-		'callbackURL': 'http://161.202.33.157:3000/auth/google/callback'
+		'callbackURL': 'http://nodejs.gpat.vn:3000/auth/google/callback'
 	}
 }
