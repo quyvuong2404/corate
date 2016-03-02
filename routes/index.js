225ac8c946a2162416b8fd05cb1e9487814f258f
nthhtn <thehien115@gmail.com>
2016-03-02 09:46:41 +0700

    Editing convention

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,6 @@ var quotes = require('../model/quotes');
 var user = require('../model/users');
 var auth = require('../lib/auth');
 
-var AlchemyAPI=require('../lib/alchemyapi');
-var alchemyapi=new AlchemyAPI();
-
 module.exports = function(app, passport) {
 
 	app.get('/', function(req, res) {
